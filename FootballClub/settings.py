f627d68b7921d3f832b8ff78ccccdf7cb7cfc6ae
aswandhkolangarodi <aswandhk2000@gmail.com>
2022-06-09 09:52:20 +0530

    first commit

--- FootballClub/settings.py
+++ FootballClub/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from distutils.command.config import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xwxvq=+9%(fgzdug$rvkw1hxq&#0q&a&%quep8-8r5#if#sl@)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
